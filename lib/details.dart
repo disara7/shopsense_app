bc31935dc288bbefeffa51604f703ddbf9ad236e
disara7 <129250994+disara7@users.noreply.github.com>
2024-05-08 10:57:32 +0530

    details

--- lib/details.dart
+++ lib/details.dart
@@ -1,34 +1,40 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
-import 'package:speech_to_text/speech_to_text.dart' as stt;
-import 'package:shopsense_app/shop.dart';
 
 class Details extends StatefulWidget {
-  const Details({Key? key}) : super(key: key);
+  const Details({Key? key, required this.title}) : super(key: key);
+
+  final String title;
 
   @override
   _DetailsState createState() => _DetailsState();
 }
 
 class _DetailsState extends State<Details> with SingleTickerProviderStateMixin {
-  FlutterTts flutterTts = FlutterTts();
-  stt.SpeechToText _speech = stt.SpeechToText();
-  bool _isListening = false;
-  late Timer _timer;
   late AnimationController _controller;
+  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
-    speakSentence('Helooo ');
+    Future.delayed(Duration(seconds: 6), () {
+      // speakSentence(
+      //     'Great! Heres what i found for you that match what you just described! The first dress is an orange floral knee length dress with a frilly skirt. If you want more details about this tap on the top right corner of the screen. or if you want to hear about the next dress, tap on the bottom right corner of the screen.');
+    });
+
+    // Initialize animation controller
     _controller = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 10), // Adjust the duration as needed
+      duration: Duration(seconds: 40), // Adjust the duration as needed
     )..repeat();
+
+    // Initialize rotation animation
+    _animation = Tween(begin: 0.0, end: 2 * 3.1415).animate(_controller);
   }
 
   Future<void> speakSentence(String sentence) async {
+    FlutterTts flutterTts = FlutterTts();
     await flutterTts.setLanguage('en-US');
     await flutterTts.setSpeechRate(1.0);
     await flutterTts.setVolume(1.0);
@@ -37,107 +43,99 @@ class _DetailsState extends State<Details> with SingleTickerProviderStateMixin {
     await flutterTts.speak(sentence);
   }
 
-  void startListening() {
-    setState(() {
-      _isListening = true;
-    });
-
-    _speech.listen(onResult: (result) {
-      setState(() {
-        _isListening = false;
-        String inputText = result.recognizedWords;
-        print(inputText);
-        speakSentence("Hi $inputText");
-        _timer.cancel(); // Cancel timer when speech is recognized
-      });
-    });
-
-    _timer = Timer(const Duration(seconds: 5), () {
-      stopListening();
-    });
-  }
-
-  void stopListening() {
-    setState(() {
-      _isListening = false;
-    });
-    _speech.stop();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
+    // Calculate the height of each button to fill the screen
+    final double buttonHeight = MediaQuery.of(context).size.height / 2;
+
     return Scaffold(
       body: Stack(
         children: [
-          Positioned.fill(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      speakSentence(
-                          'Helooo Jane! congratulations on successfully setting up your shopsense profile, and welcome to the shopsense store! Please describe what kind of a clothing item you are looking for, today. After you describe, I will come up with a few clothes that match your description and if the item i describe matches your desire, please tap on the top right corner of the screen to get more details about purchasing and all. Or, if you want to hear about the other options you have tap on the bottom right corner of the screen. if you want to go back to the previous item, tap on the bottom left corner of the screen, AND if you want me to repeat what i just said, you can tap on the top left corner of the screen.');
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const Shop(
-                                  title: '',
-                                )),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.all(20.0),
-                      backgroundColor: Colors.pink[100],
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0),
-                      ),
-                      minimumSize: const Size(
-                          double.infinity, 0), // Set minimum size to fill width
-                    ),
-                    child: const Text(''),
+          GridView.count(
+            shrinkWrap: true, // Ensure the GridView shrinks to fit its content
+            crossAxisCount: 2, // Number of buttons in a row
+            childAspectRatio: MediaQuery.of(context).size.width /
+                (buttonHeight * 2), // Maintain aspect ratio
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  speakSentence(
+                      'Sure, let me repeat what i just said. Please describe what kind of a clothing item you are looking for, today. After you describe, I will come up with a few clothes that match your description and if the item i describe matches your desire, please tap on the top right corner of the screen to get more details about the item. Or, if you want to hear about the other options you have tap on the bottom right corner of the screen. if you want to go back to the previous item, tap on the bottom left corner of the screen, AND if you want me to repeat what i just said, you can tap on the top left corner of the screen.');
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20.0),
+                  backgroundColor: Colors.pink[100],
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(0), // Adjust the radius as needed
                   ),
+                  minimumSize: Size(double.infinity,
+                      buttonHeight), // Set minimum size to fill height
                 ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      speakSentence(
-                          "Of course, you can say it again. Please tap on the top half of the screen if you are sure you want to continue, or you can always reanswer by tapping on the bottom half of the screen.");
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.all(20.0),
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0),
-                      ),
-                      minimumSize: const Size(
-                          double.infinity, 0), // Set minimum size to fill width
-                    ),
-                    child: const Text(''),
+                child: const Text('deets'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  speakSentence(
+                      'Excellent choice! Let me describe this dress for you. Imagine slipping into a dress thats a tactile celebration of summer: soft fabric adorned with playful frills and a lively floral pattern, all in a radiant orange hue. Its like wearing sunshine itself, with every touch revealing its vibrant warmth. This delightful creation hugs your curves gently, with frills and ruffles adding a delightful sensation to each movement. The intricate floral pattern, though unseen, can be felt like delicate petals against your skin. With this dress, youll embody the essence of summer, exuding energy and vitality wherever you go. Whether for a leisurely stroll or a gathering with friends, youll feel both stylish and comfortable, inviting admiration with every step.');
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20.0),
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(0), // Adjust the radius as needed
                   ),
+                  minimumSize:
+                      Size(double.infinity, buttonHeight), // Set button color
                 ),
-              ],
-            ),
+                child: const Text(''),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  speakSentence('Sure, you can re-describe what you wish for.');
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20.0),
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(0), // Adjust the radius as needed
+                  ),
+                  minimumSize: Size(double.infinity,
+                      buttonHeight), // Set minimum size to fill height
+                  // Set button color
+                ),
+                child: const Text(''),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  speakSentence(
+                      'The next dress i found for you is a comfortable casual dress with a smoked bust to make you look smart.');
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20.0),
+                  backgroundColor: Colors.pink[100],
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(0), // Adjust the radius as needed
+                  ),
+                  minimumSize: Size(double.infinity,
+                      buttonHeight), // Set minimum size to fill height
+                ),
+                child: const Text(''),
+              ),
+            ],
           ),
-          AnimatedBuilder(
-            animation: _controller,
-            builder: (context, child) {
-              return Transform.rotate(
-                angle: _controller.value * 2 * 3.1415,
-                child: child,
-              );
-            },
-            child: Align(
-              alignment: Alignment.center,
-              child: Image.asset(
-                'images/shopz.png',
-                width: MediaQuery.of(context).size.width / 3,
+          Positioned.fill(
+            child: Center(
+              child: RotationTransition(
+                turns: _animation,
+                child: Image.asset(
+                  'images/shopz.png',
+                  width: MediaQuery.of(context).size.width / 3,
+                  // Adjust the width as needed
+                ),
               ),
             ),
           ),
--- lib/shop.dart
+++ lib/shop.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'details.dart'; // Import the details.dart file
 
 class Shop extends StatefulWidget {
   const Shop({Key? key, required this.title}) : super(key: key);
@@ -20,7 +21,7 @@ class _ShopState extends State<Shop> with SingleTickerProviderStateMixin {
     super.initState();
     Future.delayed(Duration(seconds: 6), () {
       speakSentence(
-          'Great! Heres what i found for you that match what you just described! The first dress is an orange floral knee length dress with a frilly skirt. If you want more details about this tap on the top right corner of the screen. or if you want to hear about the next dress, tap on the bottom right corner of the screen.');
+          'Great! Here\'s what I found for you that matches what you just described! The first dress is an orange floral knee-length dress with a frilly skirt. If you want more details about this, tap on the top right corner of the screen. Or, if you want to hear about the next dress, tap on the bottom right corner of the screen.');
     });
 
     // Initialize animation controller
@@ -60,7 +61,7 @@ class _ShopState extends State<Shop> with SingleTickerProviderStateMixin {
               ElevatedButton(
                 onPressed: () {
                   speakSentence(
-                      'Sure, let me repeat what i just said. Please describe what kind of a clothing item you are looking for, today. After you describe, I will come up with a few clothes that match your description and if the item i describe matches your desire, please tap on the top right corner of the screen to get more details about the item. Or, if you want to hear about the other options you have tap on the bottom right corner of the screen. if you want to go back to the previous item, tap on the bottom left corner of the screen, AND if you want me to repeat what i just said, you can tap on the top left corner of the screen.');
+                      'Sure, let me repeat what I just said. Please describe what kind of clothing item you are looking for today. After you describe, I will come up with a few clothes that match your description, and if the item I describe matches your desire, please tap on the top right corner of the screen to get more details about the item. Or, if you want to hear about the other options you have, tap on the bottom right corner of the screen. If you want to go back to the previous item, tap on the bottom left corner of the screen, AND if you want me to repeat what I just said, you can tap on the top left corner of the screen.');
                 },
                 style: ElevatedButton.styleFrom(
                   padding: const EdgeInsets.all(20.0),
@@ -77,7 +78,7 @@ class _ShopState extends State<Shop> with SingleTickerProviderStateMixin {
               ElevatedButton(
                 onPressed: () {
                   speakSentence(
-                      'Excellent choice! Let me describe this dress for you. Imagine slipping into a dress thats a tactile celebration of summer: soft fabric adorned with playful frills and a lively floral pattern, all in a radiant orange hue. Its like wearing sunshine itself, with every touch revealing its vibrant warmth. This delightful creation hugs your curves gently, with frills and ruffles adding a delightful sensation to each movement. The intricate floral pattern, though unseen, can be felt like delicate petals against your skin. With this dress, youll embody the essence of summer, exuding energy and vitality wherever you go. Whether for a leisurely stroll or a gathering with friends, youll feel both stylish and comfortable, inviting admiration with every step.');
+                      'Excellent choice! Let me describe this dress for you. Imagine slipping into a dress that\'s a tactile celebration of summer: soft fabric adorned with playful frills and a lively floral pattern, all in a radiant orange hue. It\'s like wearing sunshine itself, with every touch revealing its vibrant warmth. This delightful creation hugs your curves gently, with frills and ruffles adding a delightful sensation to each movement. The intricate floral pattern, though unseen, can be felt like delicate petals against your skin. With this dress, you\'ll embody the essence of summer, exuding energy and vitality wherever you go. Whether for a leisurely stroll or a gathering with friends, you\'ll feel both stylish and comfortable, inviting admiration with every step.');
                 },
                 style: ElevatedButton.styleFrom(
                   padding: const EdgeInsets.all(20.0),
@@ -108,22 +109,34 @@ class _ShopState extends State<Shop> with SingleTickerProviderStateMixin {
                 ),
                 child: const Text(''),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  speakSentence(
-                      'The next dress i found for you is a comfortable casual dress with a smoked bust to make you look smart.');
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Details(
+                        title: 'Details Screen',
+                      ),
+                    ),
+                  );
                 },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.all(20.0),
-                  backgroundColor: Colors.pink[100],
-                  shape: RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(0), // Adjust the radius as needed
+                child: ElevatedButton(
+                  onPressed: () {
+                    speakSentence(
+                        'The next dress I found for you is a comfortable casual dress with a smoked bust to make you look smart.');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(20.0),
+                    backgroundColor: Colors.pink[100],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          0), // Adjust the radius as needed
+                    ),
+                    minimumSize: Size(double.infinity,
+                        buttonHeight), // Set minimum size to fill height
                   ),
-                  minimumSize: Size(double.infinity,
-                      buttonHeight), // Set minimum size to fill height
+                  child: const Text(''),
                 ),
-                child: const Text(''),
               ),
             ],
           ),
